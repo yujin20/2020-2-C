a3ea2349dcdbea3c78a9dd3928e290beb26c1f5e
yujin20 <76104746+yujin20@users.noreply.github.com>
2020-12-25 01:42:45 +0900

    chapter 7 files

---
+++ repetition.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	//동일한 문자열 반복
+	printf("C 언어 재미있네요!\n");
+	printf("C 언어 재미있네요!\n");
+	printf("C 언어 재미있네요!\n");
+
+	int n = 1;
+	//정수값을 1씩 증가시키면서 출력 반복
+	printf("%d\n", n++);
+	printf("%d\n", n++);
+	printf("%d\n", n++);
+	printf("%d\n", n++);
+	printf("%d\n", n++);
+
+	return 0;
+}
---
+++ whilenumber.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#define MAX 5
+
+int main(void)
+{
+	int n = 1;
+
+	while (n <= MAX)
+		printf("%d\n", n++);
+
+	printf("\n제어변수 n => %d\n", n);
+
+	return 0;
+}
